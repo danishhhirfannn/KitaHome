80cafa9d21b5adf66395df60baf14b9754d75e53
danishhhirfannn <danishirfan.workspace@gmail.com>
2025-03-05 17:48:52 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,59 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view/> -->
+
+  <!-- <main>
+    <v-btn></v-btn>
+  </main> -->
+  <v-app>
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer app v-model="drawer" floating permanent>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">KitaHome</v-list-item-title>
+            <v-list-item-title class="text-overline text-left mt-3">Main Menu</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list dense>
+          <v-list-item v-for="(item, index) in items" :key="index" :to="item.to" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Main Content -->
+    <v-main>
+      <v-container>
+        <router-view />
+        <v-btn color="primary">Click Me</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Home", icon: "mdi-home", to: "/" },
+        { title: "About", icon: "mdi-information", to: "/about" },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-sheet>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
